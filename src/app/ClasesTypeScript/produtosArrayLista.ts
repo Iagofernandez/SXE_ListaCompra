3aef2aca44f304546efff8d3fb595dd93e380e7e
Iago <ifernandezblanco@danielcastelao.org>
2020-03-02 09:08:55 +0100

    Cambios realizados en el formao de la pagina, cambios en html y creacion del array de produtos

---
+++ src/app/ClasesTypeScript/produtosArrayLista.ts
@@ -0,0 +1,9 @@
+import {Productos} from './productos';
+
+export const PRODUCTOS_ARRAY: Productos[] = [
+
+  {nombre: 'Teclado', unidades: 2, precio: 100},
+  {nombre: 'Monitor', unidades: 1, precio: 200},
+  {nombre: 'Raton', unidades: 5, precio: 25},
+
+];
