c005330588e5434ad6dfadc1bf8646dde98dff4f
Iago <ifernandezblanco@danielcastelao.org>
2020-03-02 09:14:33 +0100

    Actualizados tanto el HTML como el Array de productos

--- src/app/ClasesTypeScript/produtosArrayLista.ts
+++ src/app/ClasesTypeScript/produtosArrayLista.ts
@@ -2,8 +2,8 @@ import {Productos} from './productos';
 
 export const PRODUCTOS_ARRAY: Productos[] = [
 
-  {nombre: 'Teclado', unidades: 2, precio: 100},
-  {nombre: 'Monitor', unidades: 1, precio: 200},
-  {nombre: 'Raton', unidades: 5, precio: 25},
+  {nombre: 'Teclado', desc: 'Hp'},
+  {nombre: 'Monitor', desc: 'Lg'},
+  {nombre: 'Raton', desc: 'Apple'},
 
 ];
